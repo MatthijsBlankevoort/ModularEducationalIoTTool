58513b90a258e2c7906cb8d9974820a2e60e0fb8
Michael van de Reep <mjvandereep@gmail.com>
2017-04-13 11:22:37 +0200

    Added redirect to login afther page load

---
+++ Website/index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+    <title> redirecting</title>
+    </head>
+    <body onload="show();">
+    <script language="JavaScript">
+	
+    <!--
+    function show(){
+         document.location.href ="LoginPage.php";
+    }
+    -->
+    </script>
+    <form name="form1">
+    <br>
+	<h1>if you are not redirected in 5 sec press the button :).</h1>
+    <input type="button" name="sethref" value="Redirect" onClick='show()'>
+    <br>
+    </form>
+    </body>
+    </html>
