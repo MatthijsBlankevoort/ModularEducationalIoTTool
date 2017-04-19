f0a8c3efd179c175df04b1a5e7e0a7c342e267d2
Kah Kit Zheng <Kah.Kit.Zheng@hva.nl>
2017-04-19 15:57:01 +0200

    Navbar collapse

--- Website/ActuatorPage.php
+++ Website/ActuatorPage.php
@@ -3,46 +3,52 @@
 <head>
     <title>Actuator Page</title>
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-    <nav class="navbar navbar-inverse navbar-fixed-top">
+    <nav class="navbar navbar-inverse">
         <div class="container-fluid">
             <div class="navbar-header">
                 <a class="navbar-brand" href="Dashboard.html">IoT Workshop</a>
             </div>
-            <ul class="nav navbar-nav">
-                <li><a href="Dashboard.html">Dashboard</a></li>
-                <li><a href="SensorPage.php">Sensors</a></li>
-                <li class="active"><a href="ActuatorPage.php">Actuators</a></li>
-                <li><a href="VisualizationPage.html">Visualizations</a></li>
-                <li><a href="ConfigurationPage.html">Configurations</a></li>
-            </ul>
+            <button type="button" class="navbar-toggle collapse" data-toggle="collapse"
+                    data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li><a href="Dashboard.html">Dashboard</a></li>
+                    <li><a href="SensorPage.php">Sensors</a></li>
+                    <li class="active"><a href="ActuatorPage.php">Actuators</a></li>
+                    <li><a href="VisualizationPage.html">Visualizations</a></li>
+                    <li><a href="ConfigurationPage.html">Configurations</a></li>
+                </ul>
+            </div>
         </div>
     </nav>
-		   <div class="container">
+    <div class="container">
         <div class="row">
-<?php
-  require_once('config.php');
-  require_once('database.php');
-  
+            <?php
+            require_once('config.php');
+            require_once('database.php');
 
-	$stmt = $con_db->prepare("select Actuator_Type from Actuator where Actuator_active = '1';");
-	// Next fire the sql statmend at the db with the first device.
-	$stmt->execute();
-	//store the results in the form of an string in result and filter only the first colum out
-	$result = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
-	echo ('<br><br><br><br><br><br><br><br>');
-	for($i = 0; $i < ($stmt->rowCount()); $i++) 
-	{
-		echo $result[$i];
-		echo ('<br>');
-		
-	}
-			
 
-?>	
-		
-		</div>
+            $stmt = $con_db->prepare("select Actuator_Type from Actuator where Actuator_active = '1';");
+            // Next fire the sql statmend at the db with the first device.
+            $stmt->execute();
+            //store the results in the form of an string in result and filter only the first colum out
+            $result = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
+            echo('<br><br><br><br><br><br><br><br>');
+            for ($i = 0; $i < ($stmt->rowCount()); $i++) {
+                echo $result[$i];
+                echo('<br>');
+
+            }
+            ?>
+        </div>
     </div>
 </body>
 </html>
--- Website/SensorPage.php
+++ Website/SensorPage.php
@@ -3,44 +3,55 @@
 <head>
     <title>Sensor Page</title>
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </head>
 <body>
     <nav class="navbar navbar-inverse navbar-fixed-top">
         <div class="container-fluid">
             <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapse" data-toggle="collapse"
+                        data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
                 <a class="navbar-brand" href="Dashboard.html">IoT Workshop</a>
             </div>
-            <ul class="nav navbar-nav">
-                <li><a href="Dashboard.html">Dashboard</a></li>
-                <li class="active"><a href="SensorPage.php">Sensors</a></li>
-                <li><a href="ActuatorPage.php">Actuators</a></li>
-                <li><a href="VisualizationPage.html">Visualizations</a></li>
-                <li><a href="ConfigurationPage.html">Configurations</a></li>
-            </ul>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li><a href="Dashboard.html">Dashboard</a></li>
+                    <li class="active"><a href="SensorPage.php">Sensors</a></li>
+                    <li><a href="ActuatorPage.php">Actuators</a></li>
+                    <li><a href="VisualizationPage.html">Visualizations</a></li>
+                    <li><a href="ConfigurationPage.html">Configurations</a></li>
+                </ul>
+            </div>
         </div>
     </nav>
 	   <div class="container">
         <div class="row">
-<?php
-  require_once('config.php');
-  require_once('database.php');
-  
 
-	$stmt = $con_db->prepare("Select Sensor_type from Sensor where Sensor_active = '1';");
-	// Next fire the sql statmend at the db with the first device.
-	$stmt->execute();
-	//store the results in the form of an string in result and filter only the first colum out
-	$result = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
-	echo ('<br><br><br><br><br><br><br><br>');
-	for($i = 0; $i < ($stmt->rowCount()); $i++) 
-	{
-		echo $result[$i];
-		echo ('<br>');
-		
-	}
-			
+        <?php
+          require_once('config.php');
+          require_once('database.php');
 
-?>	
+
+            $stmt = $con_db->prepare("Select Sensor_type from Sensor where Sensor_active = '1';");
+            // Next fire the sql statmend at the db with the first device.
+            $stmt->execute();
+            //store the results in the form of an string in result and filter only the first colum out
+            $result = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
+            echo ('<br><br><br><br><br><br><br><br>');
+            for($i = 0; $i < ($stmt->rowCount()); $i++)
+            {
+                echo $result[$i];
+                echo ('<br>');
+
+            }
+
+
+        ?>
 		
 		</div>
     </div>
