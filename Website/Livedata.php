48316d17fa57713d8a002833d61046b4d45d3081
Matthijs Blankevoort <Matthijsblankevoort@hotmail.com>
2017-05-04 16:39:22 +0200

    Added PHP for date
    -Date wordt nu ook uit de database geladen.
    -SQL query verbeterd
    TODO: REAL TIME DATA W AJAX

--- Website/Livedata.php
+++ Website/Livedata.php
@@ -16,15 +16,27 @@
 	require_once('config.php');
 	require_once('database.php');
   
-	$stmt = $con_db->prepare("select * from Sensor_Log where Sensor_ID = '001' ORDER BY Sensor_Timestamp limit 4;");
+	$stmt = $con_db->prepare("select * from Sensor_Log where Sensor_ID = '001' ORDER BY Sensor_Timestamp DESC limit 4;");
 	// Next fire the sql statmend at the db with the first device.
 	$stmt->execute();
-	//store the results in the form of an string in result and filter only the first colum out
-	$startresult = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
+	//store the sensorValues in the form of an string in sensorValue and filter only the first colum out
+	$startSensorValue = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
 
-	$stmt = $con_db->prepare("select * from Sensor_Log where Sensor_ID = '001' ORDER BY Sensor_Timestamp limit 1;");
+        $stmt = $con_db->prepare("select * from Sensor_Log where Sensor_ID = '001' ORDER BY Sensor_Timestamp DESC limit 4;");
+    // Next fire the sql statmend at the db with the first device.
+    $stmt->execute();
+    $startTimestamp = $stmt->fetchAll(PDO::FETCH_COLUMN, 1);
+
+	$stmt = $con_db->prepare("select * from Sensor_Log where Sensor_ID = '001' ORDER BY Sensor_Timestamp DESC limit 1;");
 	$stmt->execute();
-	$result = $stmt->fetch(PDO::FETCH_COLUMN, 0);
+	$sensorValue = $stmt->fetch(PDO::FETCH_COLUMN, 0);
+
+        $stmt = $con_db->prepare("select Sensor_Timestamp from Sensor_Log where Sensor_ID = '001' ORDER BY Sensor_Timestamp DESC limit 1;");
+    $stmt->execute();
+    $timestamp = $stmt->fetch(PDO::FETCH_COLUMN, 0);
+
+
+
 ?>
 <!-- 
 <script type="text/javascript">
@@ -34,38 +46,43 @@
 
 <script type="text/javascript">
 
-	console.log(<?php echo json_encode($startresult); ?>);
+	
+    console.log(<?php echo json_encode($startTimestamp);?>);
 	var chart = c3.generate({
     data: {
         x: 'x',
+        xFormat: '%Y-%m-%d %H:%M:%S', 
         columns: [
-            ['x', '2012-12-28', '2012-12-29', '2012-12-30', '2012-12-31'],
-            ['test', <?php echo json_encode($startresult); ?>[0], <?php echo json_encode($startresult); ?>[1], <?php echo json_encode($startresult); ?>[2], <?php echo json_encode($startresult); ?>[3]],
+            ['x', <?php echo json_encode($startTimestamp); ?>[3], <?php echo json_encode($startTimestamp); ?>[2], <?php echo json_encode($startTimestamp); ?>[1], <?php echo json_encode($startTimestamp); ?>[0]],
+            ['test', <?php echo json_encode($startSensorValue); ?>[0], <?php echo json_encode($startSensorValue); ?>[1], <?php echo json_encode($startSensorValue); ?>[2], <?php echo json_encode($startSensorValue); ?>[3]],
         ]
     },
     axis: {
         x: {
-            type: 'timeseries',
+            type: 'category',
             tick: {
-                format: '%m/%d',
+                format: '%Y-%m-%d %H:%M:%S',
             }
         }
     },
 
 
-});
+})
+;
 
-var date = new Date ("2013-01-01");
+
+// console.log(<?php echo json_encode($timestamp)?>);
 setInterval(function () {
+var timestamp = <?php echo json_encode($timestamp)?>;
 
     chart.flow({
         columns: [
-            ['x', new Date (date)],
-            ['test', <?php echo json_encode($result); ?>],
+            ['x', timestamp],
+            ['test', <?php echo json_encode($sensorValue); ?>],
         ],
-        duration: 750,
+        duration: 1000,
     });
-    date.setDate(date.getDate() + 1);
+     timestamp = timestamp + 1000;
 }, 2000);
 
 </script>
