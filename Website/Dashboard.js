76685225cb2a4a1fb30121ffc7f7a318bcba69b4
Matthijs Blankevoort <Matthijsblankevoort@hotmail.com>
2017-05-11 16:09:13 +0200

    Added CSS to dashboard page
    Pretty dashboard :)

---
+++ Website/Dashboard.js
@@ -0,0 +1,43 @@
+
+$(document).ready(function(){
+	$(".button1").mouseover(function(){
+		$(".button1 i").toggleClass("invisible");
+		$(".button1 span").toggleClass("invisible");
+		$(".button1 hr").toggleClass("invisible");
+	});
+	$(".button1").mouseout(function(){
+		$(".button1 i").toggleClass("invisible");
+		$(".button1 span").toggleClass("invisible");
+		$(".button1 hr").toggleClass("invisible");
+	});
+	$(".button2").mouseover(function(){
+		$(".button2 i").toggleClass("invisible");
+		$(".button2 span").toggleClass("invisible");
+		$(".button2 hr").toggleClass("invisible");
+	});
+	$(".button2").mouseout(function(){
+		$(".button2 i").toggleClass("invisible");
+		$(".button2 span").toggleClass("invisible");
+		$(".button2 hr").toggleClass("invisible");
+	});
+	$(".button3").mouseover(function(){
+		$(".button3 i").toggleClass("invisible");
+		$(".button3 span").toggleClass("invisible");
+		$(".button3 hr").toggleClass("invisible");
+	});
+	$(".button3").mouseout(function(){
+		$(".button3 i").toggleClass("invisible");
+		$(".button3 span").toggleClass("invisible");
+		$(".button3 hr").toggleClass("invisible");
+	});
+	$(".button4").mouseover(function(){
+		$(".button4 i").toggleClass("invisible");
+		$(".button4 span").toggleClass("invisible");
+		$(".button4 hr").toggleClass("invisible");
+	});
+	$(".button4").mouseout(function(){
+		$(".button4 i").toggleClass("invisible");
+		$(".button4 span").toggleClass("invisible");
+		$(".button4 hr").toggleClass("invisible");
+	});
+});
