98825be1171cc093dd9c236dde6c97144afde9ec
Matthijs Blankevoort <Matthijsblankevoort@hotmail.com>
2017-05-24 12:01:26 +0200

    Changed JS selector

--- Website/Dashboard.js
+++ Website/Dashboard.js
@@ -1,11 +1,11 @@
 
 $(document).ready(function(){
-	$("button").mouseover(function(){
+	$(".fancybutton").mouseover(function(){
 		$(this.getElementsByTagName("i")).toggleClass("invisible");
 		$(this.getElementsByTagName("span")).toggleClass("invisible");
 		$(this.getElementsByTagName("hr")).toggleClass("invisible");
 	});
-	$("button").mouseout(function(){
+	$(".fancybutton").mouseout(function(){
 		$(this.getElementsByTagName("i")).toggleClass("invisible");
 		$(this.getElementsByTagName("span")).toggleClass("invisible");
 		$(this.getElementsByTagName("hr")).toggleClass("invisible");
